7633a24c1ea2731d3cc9033fa90c064103c82643
hijiri <web_company@hotmail.com>
2010-05-16 02:17:17 +0900

    showVisitorCount : Loggix Pulgin

---
+++ readme.txt
@@ -0,0 +1,46 @@
+/**
+ * Loggix_Plugin - Show Visitor Count
+ *
+ * @copyright Copyright (C) UP!
+ * @author    hijiri
+ * @link      http://tkns.homelinux.net/
+ * @license   http://www.opensource.org/licenses/bsd-license.php  New BSD License
+ * @since     2010.04.24
+ * @version   10.5.8
+ */
+
+●訪問者数とオンラインユーザ数を表示するプラグイン
+
+■概略
+このソフトウェアは、Loggixを使用したサイトの訪問者数とオンラインユーザ数を表示するプラグインです。
+
+■詳細
+訪問者数とオンラインユーザ数を表示するだけの極々シンプルなカウンターです。
+
+同日内の同一IPアドレスからは1度しかカウントアップしません。指定した時間内に有ったアクセスをオンラインユーザとして扱います。
+
+■インストール/アンインストール方法
+インストール
+    1./plugins/へshowVisitorCount.phpをアップロードします。必要であれば、return しているHTMLを修正してください。
+    2./data/へshowVisitorCount.dbをアップロードし、読み書き出来るパーミッションに変更します。
+    3.<?php echo showVisitorCount();?>をテンプレートファイルの表示したい個所へ追加します。
+アンインストール
+    1./plugins/からshowVisitorCount.phpを削除します。
+    2./data/からshowVisitorCount.dbを削除します。
+    3.<?php echo showVisitorCount();?>をテンプレートファイルから削除します。
+
+■使用方法
+カウンター数値は1からスタートします。任意の数値からスタートしたい場合は、showVisitorCount.dbに対して以下のsqliteコマンドを実行してください。
+
+UPDATE count_log SET count = '任意の数値';
+
+■その他
+このプラグインは、/plugins/へ配置すれば自動で適用される通常の動作とは違い、<?php echo showVisitorCount();?>を記入したテンプレートファイルが読み込まれる度に動作します。
+
+したがって、Loggix内の全ての訪問者とオンラインユーザをカウントする場合は、/theme/base.htmlへ<?php echo showVisitorCount();?>を追加するのが良いでしょう。任意のページのみをカウントする場合は、それに対応したテンプレートファイルに追加してください。
+
+■サポート
+作者多忙の為サポート出来ません。意見/感想はContactからご連絡ください。
+
+■更新履歴
+2010-05-09:公開
---
+++ showVisitorCount.php
@@ -0,0 +1,150 @@
+<?php
+/**
+ * Loggix_Plugin - Show Visitor Count
+ *
+ * @copyright Copyright (C) UP!
+ * @author    hijiri
+ * @link      http://tkns.homelinux.net/
+ * @license   http://www.opensource.org/licenses/bsd-license.php  New BSD License
+ * @since     2010.04.24
+ * @version   10.5.9
+ */
+
+// $this->plugin->addFilter('navigation', 'showVisitorCount');
+
+function showVisitorCount()
+{
+    global $pathToIndex;
+
+    // SETTING BEGIN
+    // Online User time (minute)
+    $limit      = 5;
+    // DataBase file
+    $fileDB      = 'sqlite:' . $pathToIndex . '/data/showVisitorCount.db';
+    // SETTING END
+
+    try {
+
+        $dbh = new PDO($fileDB);
+
+        // Get Date (DB)
+        $result = $dbh->query('SELECT date FROM count_log');
+        $dateLog  = $result->fetchColumn();
+
+        // Get Today
+        $time = time();
+        $dateNow = date('Y/m/d', $time);
+
+        // Different day
+        if ($dateLog !== $dateNow) {
+
+            // initialize the access_log
+            $sth = $dbh->prepare('DELETE FROM access_log');
+            $sth->execute();
+        
+            // Update date
+            $sql = 'UPDATE count_log '
+                 . 'SET '
+                 . "date = '" . $dateNow . "'";
+            $sth = $dbh->prepare($sql);
+            $sth->execute();
+        }
+
+        // Get host name
+        $host = (isset($_SERVER['REMOTE_HOST'])) ? $_SERVER['REMOTE_HOST'] : '';
+        $addr = $_SERVER['REMOTE_ADDR'];
+        if (($host == '') || ($host == $addr)) { $host = gethostbyaddr($addr); }
+        if ($host == '') { $host = $addr; }
+
+        // Check already access
+        $result = $dbh->query('SELECT host FROM access_log');
+        if ($item = $result->fetchAll()) {
+            foreach ($item as $row) {
+                $accessList[] = $row['host'];
+            }
+            $countFlag = (in_array($host, $accessList)) ? FALSE : TRUE;
+
+        // First access
+        } else {
+            $countFlag = TRUE;
+        }
+
+        // Get online user
+        $result = $dbh->query('SELECT * FROM online_log');
+        $item   = $result->fetchAll();
+        foreach ($item as $row) {
+            $onlineList[] = array('time' =>$row['time'], 'online_host' =>$row['online_host']);
+        }
+
+        // Delete over limit
+        $i = 0;
+        $limit *= 60;
+        foreach ($onlineList as $row) {
+            if (($time - $limit) > $row['time']) {
+                array_splice($onlineList,$i);
+                last;
+            }
+            $i++;
+        }
+    
+        // Check already access
+        $onlineFlag = TRUE;
+        foreach ($onlineList as $row) {
+            if (in_array($host, $row)) {
+                $onlineFlag = FALSE;
+                last;
+            }
+        }
+
+        // Add new online user
+        if ($onlineFlag) {
+            array_unshift($onlineList, array('time'=>$time,'online_host'=>$host));
+        }
+    
+        // Read count
+        $result = $dbh->query('SELECT count FROM count_log');
+        $count  = $result->fetchColumn();
+        
+        // Count up
+        if ($countFlag) {
+            // Increment
+            $count++;
+
+            // Add access
+            $sql = 'INSERT INTO access_log '
+                 . "VALUES('" . $host . "')";
+            $sth = $dbh->prepare($sql);
+            $sth->execute();
+
+            // Update count
+            $sql = 'UPDATE count_log '
+                 . 'SET '
+                 . "count = '" . $count . "'";
+            $sth = $dbh->prepare($sql);
+            $sth->execute();
+        }
+
+        // Save online user list
+        $sth = $dbh->prepare('DELETE FROM online_log');
+        $sth->execute();
+        foreach ($onlineList as $row) {
+            $sql = 'INSERT INTO online_log '
+                 . "VALUES('" . $row['time'] . "', '" . $row['online_host'] . "')";
+            $sth = $dbh->prepare($sql);
+            $sth->execute();
+        }
+
+        // Get online user count
+        $online = count($onlineList);
+
+    } catch (PDOException $exception) {
+        die($exception->getMessage());
+    }
+
+    $users = ($online == 1) ? 'user' : 'users';
+
+    return '
+<p>Visitor:' . $count . 'th.</p>
+<p>Online:' . $online . $users . '.</p>
+';
+}
